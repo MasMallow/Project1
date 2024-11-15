a9416af6b5a4c64b02873c9b07bace8e6b15103d
Phuwadet <102339087+MasMallow@users.noreply.github.com>
2024-11-15 14:45:03 +0700

    15/11/2024

--- my-project/src/routes/homeRoute.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async function homeRoute(app: FastifyInstance) {
-  app.get('/', async (request, reply) => {
-    return { message: 'Home Page' };
-  });
-}
